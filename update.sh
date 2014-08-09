e7542f3e4a49c3e8d84832885c1e6482c0b85479
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2014-08-09 19:09:31 +0200

    feat: mocha, expect.js and some JavaScript templates

--- update.sh
+++ update.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 cp ~/Library/Preferences/WebStorm8/templates/CommonJS.xml ./templates/
+cp ~/Library/Preferences/WebStorm8/templates/Expect.js.xml ./templates/
 cp ~/Library/Preferences/WebStorm8/templates/Gulp.xml ./templates/
+cp ~/Library/Preferences/WebStorm8/templates/JavaScript.addon.xml ./templates/
+cp ~/Library/Preferences/WebStorm8/templates/Mocha.xml ./templates/
